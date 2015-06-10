75138f14c1d486c614059cb0fef08f1320c5299d
Zhenbao Zhai <arthaszhai@gmail.com>
2015-06-10 15:32:07 +0800

    a

--- beauty-utils/src/main/java/cn/beauty/modules/utils/A.java
+++ beauty-utils/src/main/java/cn/beauty/modules/utils/A.java
@@ -1,13 +1,14 @@
-package cn.beauty.utils;
+package cn.beauty.modules.utils;
+
+import java.util.HashMap;
 
 public class A {
 	public static void main(String[] args) {
 		System.out.println("main");
 		System.out.println("masssssin"); 
 		System.out.println("masssssin"); 
-		System.out.println("masssssin"); 
-		System.out.println("masssssin"); 
-		System.out.println("masssssin"); 
-		System.out.println("masssssin"); 
+		HashMap<String, String> hashMap=new HashMap<String, String>();
+		hashMap.put("rule", "checked");
+		
 	}
 }
--- beauty-utils/src/main/java/cn/beauty/modules/utils/B.java
+++ beauty-utils/src/main/java/cn/beauty/modules/utils/B.java
@@ -1,4 +1,4 @@
-package cn.beauty.utils;
+package cn.beauty.modules.utils;
 
 public class B {
 	public static void main(String[] args) {
